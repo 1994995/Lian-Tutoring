80a757ce01689a8d07369243f0b92b7031d3ff98
LianLambert <lian1lambert@gmail.com>
2023-05-19 18:45:24 -0400

    added more corgi photos

--- colorChanger.js
+++ colorChanger.js
@@ -1,4 +1,9 @@
 const separators = document.getElementsByClassName("separator");
+const images = document.getElementsByClassName("image");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+let current = 0;
+let numImg = images.length;
 
 
 for (let sep of separators) {
@@ -11,6 +16,46 @@ for (let sep of separators) {
     }
   });
 }
+
+prev.addEventListener('click', () => {
+  if (current != 0) {
+    current -= 1;
+  }
+  else {
+    current = numImg-1;
+  }
+
+  for (let i=0; i<numImg; i++) {
+    const img = images[i];
+    if (!img.classList.contains("hide")) {
+      img.classList.add("hide")
+    }
+    if (i==current) {
+      img.classList.remove("hide");
+    }
+  }
+})
+
+next.addEventListener('click', () => {
+  if (current != numImg -1) {
+    current += 1;
+  }
+  else {
+    current = 0;
+  }
+
+  for (let i=0; i<numImg; i++) {
+    const img = images[i];
+    if (!img.classList.contains("hide")) {
+      img.classList.add("hide")
+    }
+    if (i==current) {
+      img.classList.remove("hide");
+    }
+  }
+})
+
+
 
 
 
